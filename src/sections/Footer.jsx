6c596b8a44a441c0b0f4725aec39b8e40b1d4fb0
VikramanMohan <vikramvkmrd007@gmail.com>
2025-04-19 12:38:17 +0530

    add

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,13 +1,10 @@
 import { SlSocialTwitter, SlSocialInstagram, SlSocialLinkedin } from "react-icons/sl";
 
-
-
-
 const Footer = () => {
   return (
     <footer className="bg-[#232227] py-10">
         <div className="container flex flex-col items-center justify-between px-8 mx-auto lg:px-2 lg:flex-row lg:w-4/5">
-        <h2 className="text-2xl font-bold text-black uppercase "> Sneaker <span className="text-2xl font-bold text-white uppercase">Spot</span> </h2>
+        <h2 className="text-2xl font-bold text-black uppercase cursor-pointer"> Sneaker <span className="text-2xl font-bold text-white uppercase">Spot</span> </h2>
             <ul className="flex flex-col items-center justify-between p-4 lg:flex-row xl:gap-12 gap-x-4 gap-y-2">
                 <a href="#" className="leading-normal no-underline text-white text-lg hover:text-[#ff626d]">Home</a>
                 <a href="#" className="leading-normal no-underline text-white text-lg hover:text-[#ff626d]">Catalog</a>
