b3be130a2aada7fa883ff9f088c8a5b6aa0e039f
VikramanMohan <vikramvkmrd007@gmail.com>
2025-07-11 17:02:55 +0530

    scroll update

--- src/display/Main.jsx
+++ src/display/Main.jsx
@@ -3,7 +3,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Navbar from "../navbar/Navbar";
 import Footer from "../sections/Footer";
-import AnimatedOutlet from "../animates/AnimatedOutlet"; // <- New import
+import AnimatedOutlet from "../animates/AnimatedOutlet";
+import ScrollToTop from "../components/ScrollToTop"; 
 
 const Main = () => {
   useEffect(() => {
@@ -13,6 +14,7 @@ const Main = () => {
   return (
     <>
       <Navbar />
+      <ScrollToTop /> 
       <AnimatedOutlet />
       <Footer />
     </>
